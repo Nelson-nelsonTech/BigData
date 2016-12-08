5de3695e027f4a80f931286313a832894e872f36
NelsonNK <nkomonl@gmail.com>
2016-12-08 07:31:16 +0100

    Added th zoom chat and timeline

---
+++ Primefaces_Graph_And_TabsMenu/src/view/ChartView2.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import org.primefaces.model.chart.PieChartModel;
+ 
+@ManagedBean
+public class ChartView2 implements Serializable {
+ 
+    private PieChartModel livePieModel;
+ 
+    public PieChartModel getLivePieModel() {
+        int random1 = (int)(Math.random() * 1000);
+        int random2 = (int)(Math.random() * 1000);
+ 
+        livePieModel.getData().put("Candidate 1", random1);
+        livePieModel.getData().put("Candidate 2", random2);
+         
+        livePieModel.setTitle("Votes");
+        livePieModel.setLegendPosition("ne");
+         
+        return livePieModel;
+    }
+     
+}
